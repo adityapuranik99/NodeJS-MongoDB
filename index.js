7bd6134e327532d19886d0ee71f746d0cd3de147
adityapuranik99 <adityapuranik99@gmail.com>
2020-10-13 13:32:52 +0530

    Node farm complete

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemp = require('./modules/replaceTemplate');
+
 ////////////////////////////////////
 // Files
 
@@ -34,20 +36,6 @@ const url = require('url');
 ////////////////////////////////////
 // SERVER
 
-const replaceTemp = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 const data = fs.readFileSync(`./1-node-farm/starter/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -60,10 +48,11 @@ const tempCard = fs.readFileSync(`./1-node-farm/starter/templates/template-card.
 // res - gives us lots of tools to send out a respons
 // req - various information about request like URL etc.
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+
+    const { query, pathname} = url.parse(req.url, true);
 
     // Overview page
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {'Content-type': 'text/html'});
         const cardsHtml = dataObj.map(ele => replaceTemp(tempCard, ele)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
@@ -71,12 +60,16 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
     // Product Page
-    } else if (pathName === '/product') {
-        res.end('This is the product');
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemp(tempProduct, product);
+
+        res.end(output);
 
 
     // API
-    } else if (pathName === '/api'){
+    } else if (pathname === '/api'){
             res.writeHead(200, {'Content-type': 'application/json'});
             res.end(data);
     // Not found
